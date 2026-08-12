45eadba7634407883137656f876a79ebc8cd27d5
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 21:57:24 +0530

    Refactor MENU_DRE.C for better readability and fixes

--- Arrays/MENU_DRE.C
+++ Arrays/MENU_DRE.C
@@ -1,87 +1,112 @@
 #include <stdio.h>
+#include <conio.h>
 
 int main()
 {
     int a[100];
     int n, choice, po, val, i;
+
     clrscr();
+
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
     printf("Enter array elements:\n");
+
+    // Take array elements from the user
     for(i = 0; i < n; i++)
     {
-	scanf("%d", &a[i]);
+        scanf("%d", &a[i]);
     }
 
     while(1)
     {
         printf("\n----- MENU -----\n");
-	printf("1. PRINT ARRAY\n");
-	printf("2. INSERT DATA\n");
-	printf("3. DELETE\n");
-	printf("4. EXIT\n");
-	printf("ENTER THE CHOICE: ");
+        printf("1. PRINT ARRAY\n");
+        printf("2. INSERT DATA\n");
+        printf("3. DELETE\n");
+        printf("4. EXIT\n");
+
+        printf("ENTER THE CHOICE: ");
         scanf("%d", &choice);
 
         switch(choice)
         {
-            case 1:   // Print Array
-		printf("ARRAY OF ELEMENTS:\n");
+            case 1:
+                // Print all elements
+                printf("ARRAY OF ELEMENTS:\n");
+
                 for(i = 0; i < n; i++)
                 {
-		    printf("%d ", a[i]);
+                    printf("%d ", a[i]);
                 }
+
                 printf("\n");
                 break;
 
-            case 2:   // Insert at specific position
-		printf("ENTER THE POICISTION: ");
-		scanf("%d", &po);
-		printf("ENTER THE VALUE");
-		scanf("%d", &val);
 
-		if(po < 1 || po > n + 1)
+            case 2:
+                // Insert a value at a given position
+                printf("ENTER THE POSITION: ");
+                scanf("%d", &po);
+
+                printf("ENTER THE VALUE: ");
+                scanf("%d", &val);
+
+                if(po < 1 || po > n + 1)
                 {
-		    printf("INVALID POICISION\n");
+                    printf("INVALID POSITION\n");
                 }
                 else
                 {
-		    for(i = n; i >= po; i--)
-		    {
-			a[i] = a[i - 1];
-		    }
-		    a[po - 1] = val;
-		    n++;
-		    printf("VALUE ADD\n");
-		}
-		break;
+                    // Shift elements right to make space
+                    for(i = n; i >= po; i--)
+                    {
+                        a[i] = a[i - 1];
+                    }
 
-	    case 3:   // Delete at specific position
-		printf("ENTER THE POSITION: ");
-		scanf("%d", &po);
+                    a[po - 1] = val;
+                    n++;
 
-		if(po < 1 || po > n)
-		{
-		    printf("INVALID POSOTIONS\n");
-		}
-		else
-		{
-		    for(i = po - 1; i < n - 1; i++)
-		    {
-			a[i] = a[i + 1];
-		    }
-		    n--;
-		    printf("VALUE DELETED\n");
-		}
-		break;
+                    printf("VALUE ADDED\n");
+                }
 
-	    case 4:   // Exit
-		printf("PROGRAM EXIT\n");
-		return 0;
+                break;
+
+
+            case 3:
+                // Delete the value at a given position
+                printf("ENTER THE POSITION: ");
+                scanf("%d", &po);
+
+                if(po < 1 || po > n)
+                {
+                    printf("INVALID POSITION\n");
+                }
+                else
+                {
+                    // Shift elements left after deletion
+                    for(i = po - 1; i < n - 1; i++)
+                    {
+                        a[i] = a[i + 1];
+                    }
+
+                    n--;
+
+                    printf("VALUE DELETED\n");
+                }
+
+                break;
+
+
+            case 4:
+                // Exit the program
+                printf("PROGRAM EXIT\n");
+                return 0;
+
 
             default:
-		printf("ARE YAAR TE KYK KHOTU NAYKHU\n");
-	}
+                printf("INVALID CHOICE\n");
+        }
     }
 }
