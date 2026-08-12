7e0bc7af0ac5cccf80a3b28d1ee8bf53b24cf875
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 21:51:38 +0530

    Refactor array access program using pointers
    Updated comments for clarity and corrected print formatting.

--- Arrays/ARRAY_~1.C
+++ Arrays/ARRAY_~1.C
@@ -1,16 +1,23 @@
-//WRITE A PROGRAM TO ACCESS ARRAY ALIMENT UNING POINTER :
+// Access array elements using a pointer
+
 #include <stdio.h>
+
 int main()
 {
-    int a[10] = {10, 20, 30, 40, 50,60,70,80,90,100};
-    int *p,i;
+    int a[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int *p, i;
 
     clrscr();
-   p = a;
+
+    // Store the address of the first array element
+    p = a;
+
+    // Access each element using pointer arithmetic
     for(i = 0; i < 10; i++)
     {
-	printf("ELEMENTS =>%d=%d\n ", i,*(p+i));
+        printf("ELEMENT => %d = %d\n", i, *(p + i));
     }
+
     getch();
     return 0;
 }
