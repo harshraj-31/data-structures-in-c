d23de5b4164f3f5aa4658b482b26fe173b9d89b9
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 21:48:54 +0530

    Update 2D_ARRAY.C

--- Arrays/2D_ARRAY.C
+++ Arrays/2D_ARRAY.C
@@ -1,37 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-  int a[3][3]=
-  {
-  {1,2,3},
-  {4,5,6},
-  {7,8,9}
-  };
-  int i,j;
-  int row,col;
-  clrscr();
-  for(i=0;i<3;i++)
-  {
-     row=0;
-     for(j=0;j<3;j++)
-     {
-     printf(" %d",a[i][j]);
-     row=row+a[i][j];
-     }
-    printf("\t %d",row);
-    printf("\n");
-  }
+    int a[3][3] =
+    {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
 
-  for(j=0;j<3;j++)
-  {
-     col=0;
-     for(i=0;i<3;i++)
-     {
-     col+=a[i][j];
-     }
-     printf(" %d",col);
-}
-	getch();
-	return 0;
-}
+    int i, j;
+    int row, col;
 
+    clrscr();
+
+    // Calculate and print each row sum
+    for(i = 0; i < 3; i++)
+    {
+        row = 0;
+
+        for(j = 0; j < 3; j++)
+        {
+            printf(" %d", a[i][j]);
+            row = row + a[i][j];
+        }
+
+        printf("\t%d", row);
+        printf("\n");
+    }
+
+    // Calculate and print each column sum
+    for(j = 0; j < 3; j++)
+    {
+        col = 0;
+
+        for(i = 0; i < 3; i++)
+        {
+            col = col + a[i][j];
+        }
+
+        printf(" %d", col);
+    }
+
+    getch();
+    return 0;
+}
