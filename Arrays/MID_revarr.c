f43ace3bde818a2f4592d858f84c79ed24f71ac2
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 21:59:42 +0530

    Update MID_revarr.c

--- Arrays/MID_revarr.c
+++ Arrays/MID_revarr.c
@@ -1,35 +1,48 @@
-//wap to reverse an array and store in another
-#include<stdio.h>
-#include<conio.h>
-int main()
+// WAP to reverse an array and store it in another array
 
+#include <stdio.h>
+#include <conio.h>
+
+int main()
 {
     int arr[10];
     int rev[10];
-    int i,j=0,n;
-    clrscr();
-    printf("\n\tEnter the array size :");
-    scanf("%d",&n);
+    int i, n;
 
-    for(i=0;i<n;i++)
+    clrscr();
+
+    printf("\n\tEnter the array size : ");
+    scanf("%d", &n);
+
+    // Take array elements
+    for(i = 0; i < n; i++)
     {
-	printf("\n\tEnter the array %d:",i);
-	scanf(" %d",&arr[i]);
+        printf("\n\tEnter the array %d: ", i);
+        scanf("%d", &arr[i]);
     }
-    for(i=0;i<n;i++)
+
+    // Store elements in reverse order
+    for(i = 0; i < n; i++)
     {
-	rev[i]=arr[n-i-1];
+        rev[i] = arr[n - i - 1];
     }
+
+    // Display original array
     printf("\n\tArray before reverse : ");
-    for(i=0;i<n;i++)
+
+    for(i = 0; i < n; i++)
     {
-	printf(" %d",arr[i]);
+        printf(" %d", arr[i]);
     }
+
+    // Display reversed array
     printf("\n\tArray after reverse : ");
-    for(i=0;i<n;i++)
+
+    for(i = 0; i < n; i++)
     {
-	printf(" %d",rev[i]);
+        printf(" %d", rev[i]);
     }
+
     getch();
     return 0;
 }
