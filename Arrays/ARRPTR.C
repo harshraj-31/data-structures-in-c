9db7a04dfb315eed9c63956c633aedb750700a22
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 21:52:11 +0530

    Update ARRPTR.C

--- Arrays/ARRPTR.C
+++ Arrays/ARRPTR.C
@@ -1,23 +1,33 @@
-// Q1. wap to acces array elements using pointer.
-// Q2. wap to allocate memeory using malloc()
+// Q1. WAP to access array elements using pointer
 
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
-	int arr[5],i;
-	int *ptr;
-	clrscr();
-	ptr = arr;
-	printf("Enter 5 elements: \n");flushall();
-	for(i=0;i<5;i++)
-	{
-		scanf("\n%d",ptr[i]);
-	}
-	for(i=0;i<5;i++)
-	{
-		printf("\n %d ",ptr[i]);
-	}
-	getch();
-	return 0;
+    int arr[5], i;
+    int *ptr;
+
+    clrscr();
+
+    // Pointer stores the address of the first array element
+    ptr = arr;
+
+    printf("Enter 5 elements:\n");
+    flushall();
+
+    // Take input using pointer
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+
+    // Display array elements using pointer
+    for(i = 0; i < 5; i++)
+    {
+        printf("\n%d", ptr[i]);
+    }
+
+    getch();
+    return 0;
 }
