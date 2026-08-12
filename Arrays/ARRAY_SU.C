1e3c0b0efe588e2bb9799c02e61c17fe9a41fc43
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 21:49:29 +0530

    Fix formatting and comments in ARRAY_SU.C

--- Arrays/ARRAY_SU.C
+++ Arrays/ARRAY_SU.C
@@ -1,21 +1,28 @@
-//ARRAY OF SUM & GIVE THE AVERAGEA::
+// Find the sum and average of 10 array elements
+
 #include <stdio.h>
+
 int main()
 {
     int i;
     int num[10] = {1,2,3,4,5,6,7,8,9,10};
     int sum = 0;
     float avg;
+
     clrscr();
+
+    // Add all array elements
     for(i = 0; i < 10; i++)
     {
-	sum = sum+num[i];
+        sum = sum + num[i];
     }
 
+    // Calculate average
     avg = sum / 10.0;
 
     printf("SUM = %d\n", sum);
     printf("AVERAGE = %.2f\n", avg);
+
     getch();
     return 0;
 }
