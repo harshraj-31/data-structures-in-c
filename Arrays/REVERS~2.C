bfd23d531131ac52dd6ff1f5b4dbe94426da3461
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-12 22:00:10 +0530

    Fix formatting and output messages in array reversal

--- Arrays/REVERS~2.C
+++ Arrays/REVERS~2.C
@@ -1,26 +1,41 @@
+// Reverse an array without using another array
+
 #include <stdio.h>
+#include <conio.h>
+
 int main()
 {
     int arr[5], n, i, temp;
+
     clrscr();
-    printf("ENTER THE ARRAY SIZE =>");
+
+    printf("ENTER THE ARRAY SIZE => ");
     scanf("%d", &n);
-    printf("ENTER THE ELMENTS =>\n");
+
+    printf("ENTER THE ELEMENTS =>\n");
+
+    // Take array elements from the user
     for(i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
-    for(i = 0; i < n/2; i++)
+
+    // Swap elements from both ends
+    for(i = 0; i < n / 2; i++)
     {
         temp = arr[i];
         arr[i] = arr[n - i - 1];
         arr[n - i - 1] = temp;
     }
-    printf("REVERSED ARRAY=>\n");
+
+    // Display the reversed array
+    printf("REVERSED ARRAY =>\n");
+
     for(i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
+
     getch();
 
     return 0;
