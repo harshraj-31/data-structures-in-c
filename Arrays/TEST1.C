38e1999b911796d9aefa69f14006ca574a46d27b
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-13 21:32:32 +0530

    Refactor TEST1.C for improved readability
    Refactor code for clarity and structure. Added comments for row and column sum calculations.

--- Arrays/TEST1.C
+++ Arrays/TEST1.C
@@ -1,40 +1,46 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
- int row,col,i,j;
- int a[3][3]=
- {
- {1,2,3},
- {4,5,6},
- {7,8,9}
- };
- clrscr();
+    int row, col, i, j;
 
- for(i=0;i<3;i++)
- {
-   row=0;
-   for(j=0;j<3;j++)
-   {
-   row=row+a[i][j];
-   printf("%d",a[i][j]);
-   }
-   printf(" SUM IS %d\n",row);
- }
+    int a[3][3] =
+    {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
 
- for(j=0;j<3;j++)
- {
-   col=0;
-   for(i=0;i<3;i++)
-   {
-   col=col+a[i][j];
-   }
-   printf(" %d",col);
- }
+    clrscr();
 
+    // Find sum of each row
+    for(i = 0; i < 3; i++)
+    {
+        row = 0;
 
+        for(j = 0; j < 3; j++)
+        {
+            row = row + a[i][j];
+            printf("%d ", a[i][j]);
+        }
 
+        printf("SUM IS %d\n", row);
+    }
 
- getch();
- return 0;
+    // Find sum of each column
+    for(j = 0; j < 3; j++)
+    {
+        col = 0;
+
+        for(i = 0; i < 3; i++)
+        {
+            col = col + a[i][j];
+        }
+
+        printf(" %d", col);
+    }
+
+    getch();
+    return 0;
 }
