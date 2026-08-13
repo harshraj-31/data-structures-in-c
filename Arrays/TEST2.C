7625e181be5185beccbdedaba2d1a4a8e5d29225
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-13 21:33:01 +0530

    Refactor array handling and pointer usage
    Refactor code to improve readability and consistency.

--- Arrays/TEST2.C
+++ Arrays/TEST2.C
@@ -1,16 +1,22 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
+    int a[5] = {10,20,30,40,50};
+    int i, *p;
 
-  int a[5]={10,20,30,40,50};
-  int i,*p;
-  clrscr();
-  p=a;
-  for(i=0;i<5;i++)
-  {
-  printf("%d %d\n",i,*(p+i));
-  }
-  getch();
-  return 0;
+    clrscr();
+
+    // Store address of the first array element
+    p = a;
+
+    // Access array elements using pointer
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d %d\n", i, *(p + i));
+    }
+
+    getch();
+    return 0;
 }
