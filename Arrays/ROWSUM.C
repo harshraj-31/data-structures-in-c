2bb8adc1ecd9b31ffc04df690aa2fd99d9e0a87b
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-13 21:30:20 +0530

    Update ROWSUM.C

--- Arrays/ROWSUM.C
+++ Arrays/ROWSUM.C
@@ -1,47 +1,69 @@
-#include<stdio.h>
-#include<conio.h>
+// Find the sum of each row and column of a 3x3 matrix
+
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
-	int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-	int i,j,r=0,c=0,csum[3],rsum[3];
-	clrscr();
-	for(i=0;i<3;i++)
-	{
-		r=0;
-		for(j=0;j<3;j++)
-		{
-			r=r+arr[i][j];
-		}
-		rsum[i]=r;
-	}
-	for(j=0;j<3;j++)
-	{
-		c=0;
-		for(i=0;i<3;i++)
-		{
-			c=c+arr[i][j];
-		}
-		csum[j]=c;
-	}
-	//prints sum of row and rows
-	for(i=0;i<3;i++)
-	{
-		printf("\n");
-		for(j=0;j<3;j++)
-		{
-			printf("\t %d",arr[i][j]);
-		}
-		printf("\t = %d",rsum[i]);
-	}
-	//prints sum of column
+    int arr[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
 
-	printf("\n\t =\t =\t =");
-	printf("\n");
-	for(j=0;j<3;j++)
-		{
-			printf("\t %d",csum[j]);
-		}
+    int i, j, r = 0, c = 0;
+    int csum[3], rsum[3];
 
-	getch();
-	return 0;
+    clrscr();
+
+    // Calculate sum of each row
+    for(i = 0; i < 3; i++)
+    {
+        r = 0;
+
+        for(j = 0; j < 3; j++)
+        {
+            r = r + arr[i][j];
+        }
+
+        rsum[i] = r;
+    }
+
+    // Calculate sum of each column
+    for(j = 0; j < 3; j++)
+    {
+        c = 0;
+
+        for(i = 0; i < 3; i++)
+        {
+            c = c + arr[i][j];
+        }
+
+        csum[j] = c;
+    }
+
+    // Print array with row sums
+    for(i = 0; i < 3; i++)
+    {
+        printf("\n");
+
+        for(j = 0; j < 3; j++)
+        {
+            printf("\t%d", arr[i][j]);
+        }
+
+        printf("\t= %d", rsum[i]);
+    }
+
+    // Print column sums
+    printf("\n\t=\t=\t=");
+    printf("\n");
+
+    for(j = 0; j < 3; j++)
+    {
+        printf("\t%d", csum[j]);
+    }
+
+    getch();
+    return 0;
 }
