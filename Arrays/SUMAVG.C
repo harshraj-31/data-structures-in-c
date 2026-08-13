8756a86b4d6064cdef4750eac78e7a3c769b34cc
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-13 21:32:01 +0530

    Improve formatting and comments in SUMAVG.C
    Refactor code for clarity and maintainability.

--- Arrays/SUMAVG.C
+++ Arrays/SUMAVG.C
@@ -1,18 +1,26 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
-	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-	int i,sum=0;
-	float avg;
-	clrscr();
-	for(i=0;i<10;i++)
-	{
-		sum += arr[i];
-	}
-	avg=sum/10.0;
-	printf("\n\tSum of array is : %d",sum);
-	printf("\n\tAvg of array is : %.2f",avg);
-	getch();
-	return 0;
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+    int i, sum = 0;
+    float avg;
+
+    clrscr();
+
+    // Add all array elements
+    for(i = 0; i < 10; i++)
+    {
+        sum += arr[i];
+    }
+
+    // Calculate average
+    avg = sum / 10.0;
+
+    printf("\n\tSum of array is : %d", sum);
+    printf("\n\tAvg of array is : %.2f", avg);
+
+    getch();
+    return 0;
 }
