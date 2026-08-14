52bd2af7ca4a47d4702e4e1d374d0b8328cf52c4
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-15 02:12:25 +0530

    Add linear probing hash table implementation
    Implemented a hash table using linear probing for collision resolution and added functionality to insert and display values.

--- Hashing/hash_table.c
+++ Hashing/hash_table.c
@@ -1,12 +1,45 @@
 /*
- * Hash Table
- * Starter template
+ * Hash Table - Linear Probing
  */
 
 #include <stdio.h>
 
+#define SIZE 10
+
 int main()
 {
-    printf("Hash Table\n");
+    int hash[SIZE];
+    int values[] = {23, 43, 13, 27, 33};
+    int n = 5;
+    int i, index;
+
+    // Mark all positions as empty
+    for(i = 0; i < SIZE; i++)
+    {
+        hash[i] = -1;
+    }
+
+    // Insert values into the hash table
+    for(i = 0; i < n; i++)
+    {
+        index = values[i] % SIZE;
+
+        // Move to the next position if collision occurs
+        while(hash[index] != -1)
+        {
+            index = (index + 1) % SIZE;
+        }
+
+        hash[index] = values[i];
+    }
+
+    // Display the hash table
+    printf("Hash Table:\n");
+
+    for(i = 0; i < SIZE; i++)
+    {
+        printf("%d -> %d\n", i, hash[i]);
+    }
+
     return 0;
 }
