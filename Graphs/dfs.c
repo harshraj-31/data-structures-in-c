729d36d37b272b30037e08fee92bc83739c2036a
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-14 22:18:23 +0530

    Update dfs.c

--- Graphs/dfs.c
+++ Graphs/dfs.c
@@ -1,12 +1,44 @@
 /*
- * Dfs
- * Starter template
+ * DFS - Depth First Search
  */
 
 #include <stdio.h>
 
+int graph[5][5] =
+{
+    {0, 1, 1, 0, 0},
+    {1, 0, 1, 1, 0},
+    {1, 1, 0, 0, 1},
+    {0, 1, 0, 0, 1},
+    {0, 0, 1, 1, 0}
+};
+
+int visited[5] = {0};
+
+// Visit one vertex and then go deeper
+void dfs(int vertex)
+{
+    int i;
+
+    printf("%d ", vertex);
+    visited[vertex] = 1;
+
+    // Visit all unvisited neighbours
+    for(i = 0; i < 5; i++)
+    {
+        if(graph[vertex][i] == 1 && visited[i] == 0)
+        {
+            dfs(i);
+        }
+    }
+}
+
 int main()
 {
-    printf("Dfs\n");
+    // Start DFS from vertex 0
+    printf("DFS Traversal: ");
+
+    dfs(0);
+
     return 0;
 }
