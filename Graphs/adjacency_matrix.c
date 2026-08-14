4d44809e0f30b88a2a72c2fec8f7fce511a10a49
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-14 21:58:38 +0530

    Update adjacency_matrix.c

--- Graphs/adjacency_matrix.c
+++ Graphs/adjacency_matrix.c
@@ -1,12 +1,33 @@
 /*
  * Adjacency Matrix
- * Starter template
  */
 
 #include <stdio.h>
 
 int main()
 {
-    printf("Adjacency Matrix\n");
+    int graph[4][4] =
+    {
+        {0, 1, 1, 0},
+        {1, 0, 1, 1},
+        {1, 1, 0, 1},
+        {0, 1, 1, 0}
+    };
+
+    int i, j;
+
+    // Print the adjacency matrix
+    printf("Adjacency Matrix:\n");
+
+    for(i = 0; i < 4; i++)
+    {
+        for(j = 0; j < 4; j++)
+        {
+            printf("%d ", graph[i][j]);
+        }
+
+        printf("\n");
+    }
+
     return 0;
 }
