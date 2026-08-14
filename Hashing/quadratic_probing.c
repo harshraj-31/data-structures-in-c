d072d4a6881bf14ef561153754926ea258fa76a6
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-15 02:14:33 +0530

    Add quadratic probing implementation in hash table
    Implemented quadratic probing for collision resolution in a hash table. Added initialization and display of the hash table.

--- Hashing/quadratic_probing.c
+++ Hashing/quadratic_probing.c
@@ -1,12 +1,48 @@
 /*
  * Quadratic Probing
- * Starter template
  */
 
 #include <stdio.h>
 
+#define SIZE 10
+
 int main()
 {
-    printf("Quadratic Probing\n");
+    int hash[SIZE];
+    int values[] = {23, 43, 13, 27, 33};
+    int n = 5;
+    int i, index;
+
+    // Mark all positions as empty
+    for(i = 0; i < SIZE; i++)
+    {
+        hash[i] = -1;
+    }
+
+    // Insert values using quadratic probing
+    for(i = 0; i < n; i++)
+    {
+        index = values[i] % SIZE;
+
+        // If collision occurs, jump using i^2
+        int j = 1;
+
+        while(hash[index] != -1)
+        {
+            index = (index + j * j) % SIZE;
+            j++;
+        }
+
+        hash[index] = values[i];
+    }
+
+    // Display the hash table
+    printf("Hash Table using Quadratic Probing:\n");
+
+    for(i = 0; i < SIZE; i++)
+    {
+        printf("%d -> %d\n", i, hash[i]);
+    }
+
     return 0;
 }
