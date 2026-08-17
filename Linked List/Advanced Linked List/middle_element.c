7344be4386c6cf850ad34698cce02df9edbd01c6
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-17 21:49:14 +0530

    Implement function to find middle element of array
    Updated the program to find and print the middle element of an array.

--- Linked List/Advanced Linked List/middle_element.c
+++ Linked List/Advanced Linked List/middle_element.c
@@ -1,12 +1,19 @@
 /*
- * Middle Element
- * Starter template
+ * Find Middle Element of an Array
  */
 
 #include <stdio.h>
 
 int main()
 {
-    printf("Middle Element\n");
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int middle;
+
+    // Find the middle index
+    middle = n / 2;
+
+    printf("Middle Element: %d", arr[middle]);
+
     return 0;
 }
