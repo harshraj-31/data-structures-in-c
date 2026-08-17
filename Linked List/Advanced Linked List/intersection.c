150bb7ac2c0c49d5480c7796f1d6c992d2c003b1
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-17 21:47:57 +0530

    Add intersection functionality for two arrays
    Implemented a program to find the intersection of two arrays.

--- Linked List/Advanced Linked List/intersection.c
+++ Linked List/Advanced Linked List/intersection.c
@@ -1,12 +1,31 @@
 /*
- * Intersection
- * Starter template
+ * Intersection of Two Arrays
  */
 
 #include <stdio.h>
 
 int main()
 {
-    printf("Intersection\n");
+    int a[] = {1, 2, 3, 4, 5};
+    int b[] = {3, 4, 5, 6, 7};
+
+    int n1 = 5, n2 = 5;
+    int i, j;
+
+    printf("Intersection: ");
+
+    // Compare each element of first array with second
+    for(i = 0; i < n1; i++)
+    {
+        for(j = 0; j < n2; j++)
+        {
+            if(a[i] == b[j])
+            {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+
     return 0;
 }
