f9aea366c4c7720cd65213f12caf2ab99d33892e
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-17 21:49:43 +0530

    Implement nth node from end in linked list

--- Linked List/Advanced Linked List/nth_node_from_end.c
+++ Linked List/Advanced Linked List/nth_node_from_end.c
@@ -1,12 +1,66 @@
 /*
  * Nth Node From End
- * Starter template
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
 
 int main()
 {
-    printf("Nth Node From End\n");
+    struct Node *head, *temp, *fast, *slow;
+    int n, i;
+
+    // Create a simple linked list
+    head = (struct Node *)malloc(sizeof(struct Node));
+    head->data = 10;
+
+    head->next = (struct Node *)malloc(sizeof(struct Node));
+    head->next->data = 20;
+
+    head->next->next = (struct Node *)malloc(sizeof(struct Node));
+    head->next->next->data = 30;
+
+    head->next->next->next = (struct Node *)malloc(sizeof(struct Node));
+    head->next->next->next->data = 40;
+
+    head->next->next->next->next = (struct Node *)malloc(sizeof(struct Node));
+    head->next->next->next->next->data = 50;
+
+    head->next->next->next->next->next = NULL;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    // Move fast pointer n positions ahead
+    fast = head;
+
+    for(i = 0; i < n; i++)
+    {
+        if(fast == NULL)
+        {
+            printf("Invalid position");
+            return 0;
+        }
+
+        fast = fast->next;
+    }
+
+    // Move both pointers until fast reaches the end
+    slow = head;
+
+    while(fast != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    printf("%dth Node From End: %d", n, slow->data);
+
     return 0;
 }
