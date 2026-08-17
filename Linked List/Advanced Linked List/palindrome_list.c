170e0a8a4f64c0ca825fefd9aa61fc35423c6fd4
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-17 21:50:13 +0530

    Implement palindrome check for integer list
    Added logic to check if a list of integers is a palindrome.

--- Linked List/Advanced Linked List/palindrome_list.c
+++ Linked List/Advanced Linked List/palindrome_list.c
@@ -1,12 +1,34 @@
 /*
  * Palindrome List
- * Starter template
  */
 
 #include <stdio.h>
 
 int main()
 {
-    printf("Palindrome List\n");
+    int arr[] = {1, 2, 3, 2, 1};
+    int n = 5;
+    int i, palindrome = 1;
+
+    // Compare elements from both ends
+    for(i = 0; i < n / 2; i++)
+    {
+        if(arr[i] != arr[n - i - 1])
+        {
+            palindrome = 0;
+            break;
+        }
+    }
+
+    // Check the result
+    if(palindrome == 1)
+    {
+        printf("List is Palindrome");
+    }
+    else
+    {
+        printf("List is Not Palindrome");
+    }
+
     return 0;
 }
