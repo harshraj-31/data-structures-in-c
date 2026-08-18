1143e839b719330a6a114019783234e86060b3f4
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-18 20:44:11 +0530

    Refactor GAME.C for circular linked list implementation
    Refactor GAME.C to include necessary headers and implement a circular linked list for removing every kth node.

--- Linked Lists/GAME.C
+++ Linked Lists/GAME.C
@@ -1,22 +1,71 @@
-#include<stdio.h>
-#incude<conio.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 
 struct node
 {
+    int data;
+    struct node *next;
+};
 
-}
 int main()
 {
-   int n,k,i;
-   clrscr();
-   printf("ENTER THE N NUMBER=>");
-   scanf("%d",&n);
+    int n, k, i;
+    struct node *start = NULL;
+    struct node *newnode, *ptr, *temp;
 
-   printf("ENTER THE K VALUE");
-   scanf("%d",&k);
+    clrscr();
 
-   for(	)
+    printf("ENTER THE N NUMBER => ");
+    scanf("%d", &n);
 
-   getch();
+    printf("ENTER THE K VALUE => ");
+    scanf("%d", &k);
 
+    // Create circular linked list
+    for(i = 1; i <= n; i++)
+    {
+        newnode = (struct node*)malloc(sizeof(struct node));
+
+        newnode->data = i;
+        newnode->next = NULL;
+
+        if(start == NULL)
+        {
+            start = newnode;
+            ptr = newnode;
+        }
+        else
+        {
+            ptr->next = newnode;
+            ptr = newnode;
+        }
+    }
+
+    // Connect last node back to first
+    ptr->next = start;
+
+    ptr = start;
+
+    // Remove every kth node
+    while(ptr->next != ptr)
+    {
+        for(i = 1; i < k - 1; i++)
+        {
+            ptr = ptr->next;
+        }
+
+        temp = ptr->next;
+        ptr->next = temp->next;
+
+        printf("Removed: %d\n", temp->data);
+
+        free(temp);
+        ptr = ptr->next;
+    }
+
+    printf("\nWinner: %d", ptr->data);
+
+    getch();
+    return 0;
 }
