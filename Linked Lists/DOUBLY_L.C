4228dff7f57e78c099537c8bbf7df0c442ffc19a
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-18 20:43:27 +0530

    Fix formatting and improve code structure in DOUBLY_L.C

--- Linked Lists/DOUBLY_L.C
+++ Linked Lists/DOUBLY_L.C
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
 
-// STRUCTURE
+// Structure for a doubly linked list node
 struct node
 {
     int data;
@@ -10,9 +10,10 @@ struct node
     struct node *prev;
 };
 
-struct node *start = NULL, *newnode, *ptr, *temp;
+struct node *start = NULL;
+struct node *newnode, *ptr, *temp;
 
-// FUNCTION DECLARATION
+// Function declarations
 void create();
 void insertstart();
 void insertend();
@@ -20,13 +21,15 @@ void deletestart();
 void deleteend();
 void display();
 
-// MAIN
+// Main function
 void main()
 {
     int choice;
+
     clrscr();
 
-    do{
+    do
+    {
         printf("\n\n===== DOUBLY LINKED LIST MENU =====\n");
         printf("1. CREATE NODE (AT END)\n");
         printf("2. INSERT AT START\n");
@@ -37,29 +40,54 @@ void main()
         printf("0. EXIT\n");
 
         printf("\nENTER YOUR CHOICE: ");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
 
         switch(choice)
         {
-            case 1: create(); break;
-            case 2: insertstart(); break;
-            case 3: insertend(); break;
-	    case 4: deletestart(); break;
-	    case 5: deleteend(); break;
-	    case 6: display(); break;
-	}
+            case 1:
+                create();
+                break;
 
-    }while(choice != 0);
+            case 2:
+                insertstart();
+                break;
+
+            case 3:
+                insertend();
+                break;
+
+            case 4:
+                deletestart();
+                break;
+
+            case 5:
+                deleteend();
+                break;
+
+            case 6:
+                display();
+                break;
+
+            case 0:
+                printf("EXITING...");
+                break;
+
+            default:
+                printf("INVALID CHOICE");
+        }
+
+    } while(choice != 0);
 
     getch();
 }
+
+// Create a node at the end
 void create()
 {
     insertend();
 }
 
-
-
+// Insert a node at the beginning
 void insertstart()
 {
     int data;
@@ -68,24 +96,25 @@ void insertstart()
 
     if(newnode == NULL)
     {
-	printf("OVERFLOW");
-	return;
+        printf("OVERFLOW");
+        return;
     }
 
     printf("ENTER DATA: ");
-    scanf("%d",&data);
+    scanf("%d", &data);
 
     newnode->data = data;
     newnode->prev = NULL;
     newnode->next = start;
 
+    // Connect the old first node with new node
     if(start != NULL)
-	start->prev = newnode;
+        start->prev = newnode;
 
     start = newnode;
 }
 
-
+// Insert a node at the end
 void insertend()
 {
     int data;
@@ -94,76 +123,88 @@ void insertend()
 
     if(newnode == NULL)
     {
-	printf("OVERFLOW");
-	return;
+        printf("OVERFLOW");
+        return;
     }
 
     printf("ENTER DATA: ");
-    scanf("%d",&data);
+    scanf("%d", &data);
 
     newnode->data = data;
     newnode->next = NULL;
 
+    // If list is empty, new node becomes first node
     if(start == NULL)
     {
-	newnode->prev = NULL;
-	start = newnode;
+        newnode->prev = NULL;
+        start = newnode;
     }
     else
     {
-	ptr = start;
+        ptr = start;
 
-	while(ptr->next != NULL)
-	    ptr = ptr->next;
+        // Move to the last node
+        while(ptr->next != NULL)
+            ptr = ptr->next;
 
-	ptr->next = newnode;
-	newnode->prev = ptr;
+        ptr->next = newnode;
+        newnode->prev = ptr;
     }
 }
 
-// DELETE START
+// Delete the first node
 void deletestart()
 {
     if(start == NULL)
     {
-	printf("LIST IS EMPTY");
-	return;
+        printf("LIST IS EMPTY");
+        return;
     }
 
     temp = start;
     start = start->next;
 
+    // Remove previous link from new first node
     if(start != NULL)
-	start->prev = NULL;
+        start->prev = NULL;
 
     printf("DELETED ELEMENT: %d", temp->data);
+
     free(temp);
 }
 
-
-// DELETE START
+// Delete the last node
 void deleteend()
 {
     if(start == NULL)
     {
-	printf("LIST IS EMPTY");
-	return;
+        printf("LIST IS EMPTY");
+        return;
     }
+
     ptr = start;
+
+    // Move to the last node
     while(ptr->next != NULL)
         ptr = ptr->next;
 
     if(ptr->prev == NULL)
+    {
+        // Only one node exists
         start = NULL;
+    }
     else
+    {
+        // Remove the last node
         ptr->prev->next = NULL;
+    }
 
     printf("DELETED ELEMENT: %d", ptr->data);
+
     free(ptr);
 }
 
-
-// DSIPLAY FUNCATION
+// Display list in both directions
 void display()
 {
     if(start == NULL)
@@ -171,16 +212,25 @@ void display()
         printf("LIST IS EMPTY");
         return;
     }
+
     ptr = start;
+
     printf("\nFORWARD: ");
+
+    // Move from first to last
     while(ptr != NULL)
     {
-	printf("%d ", ptr->data);
-	if(ptr->next == NULL)
-	    break;
-	ptr = ptr->next;
+        printf("%d ", ptr->data);
+
+        if(ptr->next == NULL)
+            break;
+
+        ptr = ptr->next;
     }
+
     printf("\nBACKWARD: ");
+
+    // Move from last to first
     while(ptr != NULL)
     {
         printf("%d ", ptr->data);
