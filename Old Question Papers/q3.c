def149a53dcebaef02a51a0231acb7f4f274a966
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-21 18:40:59 +0530

    Refactor linked list program for clarity and fixes

--- Old Question Papers/q3.c
+++ Old Question Papers/q3.c
@@ -1,19 +1,20 @@
-// 3.Write a menu driven program to penform the following operations on singly linked list using user-defined functions:
-// a) Count total nodes
-// b) Create a copy of the linked list
+// Menu driven program for counting nodes and copying a linked list
 
 #include <stdio.h>
 #include <stdlib.h>
 
+// Basic node structure
 struct node
 {
     int data;
     struct node *next;
 };
 
+// Start of the original and copied lists
 struct node *start = NULL;
-struct node *copyStart = NULL;   // head of copied list
+struct node *copyStart = NULL;
 
+// Create a copy of the linked list
 void copyList()
 {
     struct node *ptr = start;
@@ -28,12 +29,15 @@ void copyList()
 
     copyStart = NULL;
 
+    // Copy each node one by one
     while (ptr != NULL)
     {
         newnode = (struct node *)malloc(sizeof(struct node));
+
         newnode->data = ptr->data;
         newnode->next = NULL;
 
+        // Add the first node to the copied list
         if (copyStart == NULL)
         {
             copyStart = newnode;
@@ -51,13 +55,16 @@ void copyList()
     printf("\nLinked list copied successfully");
 }
 
-
+// Create a new node at the end
 void create()
 {
     struct node *newnode, *ptr;
+
     newnode = (struct node *)malloc(sizeof(struct node));
+
     printf("\nEnter the new node data: ");
     scanf("%d", &newnode->data);
+
     newnode->next = NULL;
 
     if (start == NULL)
@@ -68,16 +75,22 @@ void create()
     else
     {
         ptr = start;
+
+        // Move to the last node
         while (ptr->next != NULL)
         {
             ptr = ptr->next;
         }
+
         ptr->next = newnode;
     }
 }
+
+// Display the original list
 void display()
 {
     struct node *ptr;
+
     if (start == NULL)
     {
         printf("\nList is empty");
@@ -86,42 +99,50 @@ void display()
     else
     {
         ptr = start;
+
         while (ptr != NULL)
         {
             printf("%d ->", ptr->data);
             ptr = ptr->next;
         }
+
         printf("NULL");
     }
 }
 
+// Display the copied list
 void displayCopy()
 {
     struct node *ptr;
+
     if (copyStart == NULL)
     {
-        printf("\n No copy of a list found");
+        printf("\nNo copy of a list found");
         return;
     }
     else
-    printf("Copied Link list: ");
-    printf("\n____________________________\n");
     {
+        printf("Copied Linked List: ");
+        printf("\n____________________________\n");
+
         ptr = copyStart;
+
         while (ptr != NULL)
         {
             printf("%d ->", ptr->data);
             ptr = ptr->next;
         }
+
         printf("NULL");
     }
 }
 
-
+// Count total nodes in the original list
 void count()
 {
     struct node *ptr;
     int count = 0;
+
     if (start == NULL)
     {
         printf("\nList is empty");
@@ -130,11 +151,14 @@ void count()
     else
     {
         ptr = start;
+
+        // Traverse the list and count each node
         while (ptr != NULL)
         {
             count++;
             ptr = ptr->next;
         }
+
         printf("Total no of nodes: %d", count);
     }
 }
@@ -142,6 +166,8 @@ void count()
 int main()
 {
     int ch = -1;
+
+    // Keep showing the menu until exit
     while (ch != 0)
     {
         printf("\n1.Create");
@@ -150,8 +176,10 @@ int main()
         printf("\n4.Copy link list");
         printf("\n5.Display copied link list");
         printf("\n0.EXIT");
-        printf("\nENter your choice : ");
+
+        printf("\nEnter your choice : ");
         scanf("%d", &ch);
+
         switch (ch)
         {
         case 1:
@@ -175,8 +203,8 @@ int main()
             break;
 
         case 0:
-            exit(0);
             printf("\nExitingg...");
+            exit(0);
             break;
 
         default:
@@ -184,5 +212,6 @@ int main()
             break;
         }
     }
+
     return 0;
 }
