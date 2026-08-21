fc7725100b6e623b170c1506777b7d9a506aacce
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-08-21 18:39:29 +0530

    Refactor linked list menu program for clarity

--- Old Question Papers/q1.c
+++ Old Question Papers/q1.c
@@ -1,33 +1,41 @@
-// menu driven program link list
+// Menu driven program for linked list
 
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node structure
 struct node
 {
     int data;
     struct node *next;
 };
 
+// Start points to the first node
 struct node *start = NULL;
 
+// Function declarations
 void create();
 void display();
 void insatlast();
 void deleteNode();
+
 int main()
 {
     int ch = -1;
-    while(ch!=0)
+
+    // Keep showing the menu until user exits
+    while(ch != 0)
     {
         printf("\n1.Create");
         printf("\n2.Display");
         printf("\n3.Insert at last");
         printf("\n4.Delete at specific position: ");
         printf("\n0.EXIT");
-        printf("\nENter your choice : ");
+
+        printf("\nEnter your choice : ");
         scanf("%d",&ch);
-        switch (ch)
+
+        switch(ch)
         {
         case 1:
             create();
@@ -44,10 +52,10 @@ int main()
         case 4:
             deleteNode();
             break;
-        
+
         case 0:
-            exit(0);
             printf("\nExitingg...");
+            exit(0);
             break;
 
         default:
@@ -55,95 +63,133 @@ int main()
             break;
         }
     }
+
     return 0;
 }
+
+// Create a new node and add it to the list
 void create()
 {
     struct node *newnode, *ptr;
+
     newnode = (struct node *)malloc(sizeof(struct node));
+
     printf("\nEnter the new node data: ");
     scanf("%d", &newnode->data);
+
     newnode->next = NULL;
-    if (start == NULL)
+
+    // If list is empty, new node becomes the first node
+    if(start == NULL)
     {
         start = newnode;
     }
     else
     {
         ptr = start;
-        while (ptr->next != NULL)
+
+        // Move to the last node
+        while(ptr->next != NULL)
         {
             ptr = ptr->next;
         }
+
         ptr->next = newnode;
     }
 }
+
+// Display all nodes
 void display()
 {
     struct node *ptr;
-    if (start == NULL)
+
+    if(start == NULL)
     {
         printf("\nList is empty");
     }
     else
     {
         ptr = start;
-        while (ptr != NULL)
+
+        // Traverse the list and print each node
+        while(ptr != NULL)
         {
             printf("%d ->", ptr->data);
             ptr = ptr->next;
         }
+
         printf("NULL");
     }
 }
+
+// Insert a new node at the end
 void insatlast()
 {
     struct node *newnode, *ptr;
+
     newnode = (struct node *)malloc(sizeof(struct node));
+
     printf("\nEnter the new node data: ");
     scanf("%d", &newnode->data);
+
     newnode->next = NULL;
-    if (start == NULL)
+
+    if(start == NULL)
     {
         start = newnode;
     }
     else
     {
         ptr = start;
-        while (ptr->next != NULL)
+
+        // Go to the last node
+        while(ptr->next != NULL)
         {
             ptr = ptr->next;
         }
+
         ptr->next = newnode;
+
         printf("Node inserted at last");
     }
 }
+
+// Delete a node using its value
 void deleteNode()
 {
     struct node *ptr, *temp;
     int srchno;
-    if (start == NULL)
+
+    if(start == NULL)
     {
         printf("\nList is empty");
+        return;
     }
+
     printf("Enter the search no: ");
     scanf("%d", &srchno);
 
-    if (start->data == srchno)
+    // Delete the first node if it matches
+    if(start->data == srchno)
     {
         temp = start;
         start = start->next;
+
         free(temp);
-        printf("\nnode deleted");
+
+        printf("\nNode deleted");
         return;
     }
 
     ptr = start;
-    while (ptr->next != NULL && ptr->next->data != srchno)
+
+    // Find the node before the node to delete
+    while(ptr->next != NULL && ptr->next->data != srchno)
     {
         ptr = ptr->next;
     }
-    if (ptr->next == NULL)
+
+    if(ptr->next == NULL)
     {
         printf("\nNot found in list");
         return;
@@ -151,6 +197,8 @@ void deleteNode()
 
     temp = ptr->next;
     ptr->next = temp->next;
+
     free(temp);
+
     printf("\nNode Deleted");
 }
