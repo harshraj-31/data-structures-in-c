40d5d89739736581d6e3b4cea38f240f0c193b85
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-03 22:47:22 +0530

    Refactor SUMDIGIT.C for clarity and formatting

--- Practice Programs/SUMDIGIT.C
+++ Practice Programs/SUMDIGIT.C
@@ -1,26 +1,36 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
+
 void sum();
+
 int main()
 {
-	clrscr();
-	sum();
-	getch();
-	return 0;
+    clrscr();
+
+    sum();
+
+    getch();
+    return 0;
 }
+
 void sum()
 {
-	int n,count=0,sum=0,temp,digit;
-	printf("\nEnter the number :");
-	scanf("%d",&n);
-	temp=n;
-	while(temp != 0)
-	{
-		       digit = temp % 10;
-		       sum = sum + digit;
-		       count++;
-		       temp = temp / 10;
-	}
-	printf("\n Sum of digits is : %d",sum);
-	printf("\n Count of digits is : %d",count);
+    int n, count = 0, sum = 0, temp, digit;
+
+    printf("\nEnter the number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    // Extract each digit, add it to sum, and count the digits.
+    while(temp != 0)
+    {
+        digit = temp % 10;
+        sum = sum + digit;
+        count++;
+        temp = temp / 10;
+    }
+
+    printf("\nSum of digits is: %d", sum);
+    printf("\nCount of digits is: %d", count);
 }
