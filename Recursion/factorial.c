c4893778a107d2768bf3e361faea5e67fbd87da9
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-07 21:54:52 +0530

    Implement factorial calculation in C

--- Recursion/factorial.c
+++ Recursion/factorial.c
@@ -7,6 +7,24 @@
 
 int main()
 {
-    printf("Factorial\n");
+    int n, i;
+    long int fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        printf("Factorial is not defined for negative numbers.");
+        return 0;
+    }
+
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial of %d = %ld", n, fact);
+
     return 0;
 }
