ae04182189ed23c2fb9a1a3d1f77aa3cb8c8b1df
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-09 23:09:43 +0530

    Implement power calculation using loops

--- Recursion/power.c
+++ Recursion/power.c
@@ -1,12 +1,22 @@
-/*
- * Power
- * Starter template
- */
-
 #include <stdio.h>
 
 int main()
 {
-    printf("Power\n");
+    int base, exp, i;
+    long long result = 1;
+
+    printf("Enter base: ");
+    scanf("%d", &base);
+
+    printf("Enter exponent: ");
+    scanf("%d", &exp);
+
+    for (i = 1; i <= exp; i++)
+    {
+        result = result * base;
+    }
+
+    printf("%d to the power of %d is %lld\n", base, exp, result);
+
     return 0;
 }
