6a1262ac8ba4dc80c1de2223074980dca3386535
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-09 23:10:17 +0530

    Implement sum of digits calculation
    Added functionality to calculate the sum of digits of an integer, including handling for negative numbers.

--- Recursion/sum_of_digits.c
+++ Recursion/sum_of_digits.c
@@ -1,12 +1,28 @@
-/*
- * Sum Of Digits
- * Starter template
- */
-
 #include <stdio.h>
 
 int main()
 {
-    printf("Sum Of Digits\n");
+    int num, originalNum, remainder, sum = 0;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    // Handle negative numbers
+    if (num < 0)
+    {
+        num = -num;
+    }
+
+    while (num > 0)
+    {
+        remainder = num % 10;
+        sum = sum + remainder;
+        num = num / 10;
+    }
+
+    printf("Sum of digits of %d is %d\n", originalNum, sum);
+
     return 0;
 }
