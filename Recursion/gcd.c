d40dd77ea731e5a8efe09010151dc29b765e7f36
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-09 22:55:12 +0530

    Add GCD calculation using Euclidean algorithm
    Implemented the Euclidean algorithm to calculate the GCD of two integers.

--- Recursion/gcd.c
+++ Recursion/gcd.c
@@ -1,12 +1,23 @@
-/*
- * Gcd
- * Starter template
- */
-
 #include <stdio.h>
 
 int main()
 {
-    printf("Gcd\n");
+    int n1, n2, a, b, remainder;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &n1, &n2);
+
+    a = n1;
+    b = n2;
+
+    while (b != 0)
+    {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+
+    printf("GCD of %d and %d is %d\n", n1, n2, a);
+
     return 0;
 }
