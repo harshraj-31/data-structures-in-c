976a7451dd0fe3e71bd51dd8783ff4bb08e803d3
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-09 23:10:48 +0530

    Add recursive Tower of Hanoi function
    Implemented recursive solution for Tower of Hanoi problem.

--- Recursion/tower_of_hanoi.c
+++ Recursion/tower_of_hanoi.c
@@ -5,7 +5,42 @@
  */
 #include <stdio.h>
 
-int main() {
-    printf("Tower of Hanoi implementation placeholder.\n");
+void towerOfHanoi(int n, char source, char destination, char auxiliary)
+{
+    // Base case: If there is only 1 disk, move it directly from source to destination
+    if (n == 1)
+    {
+        printf("Move disk 1 from %c to %c\n", source, destination);
+        return;
+    }
+
+    // Step 1: Move n-1 disks from source to auxiliary peg
+    towerOfHanoi(n - 1, source, auxiliary, destination);
+
+    // Step 2: Move the nth disk from source to destination peg
+    printf("Move disk %d from %c to %c\n", n, source, destination);
+
+    // Step 3: Move the n-1 disks from auxiliary peg to destination peg
+    towerOfHanoi(n - 1, auxiliary, destination, source);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter the number of disks: ");
+    scanf("%d", &n);
+
+    if (n > 0)
+    {
+        printf("\nSequence of moves:\n");
+        // 'A' is Source, 'C' is Destination, 'B' is Auxiliary
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+    else
+    {
+        printf("Number of disks must be greater than 0.\n");
+    }
+
     return 0;
 }
