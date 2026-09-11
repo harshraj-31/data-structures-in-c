22cb7132806826d4c322e4bf315fdb86476191b4
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-11 22:22:21 +0530

    Refactor linear search implementation in linear.c

--- Searching/linear.c
+++ Searching/linear.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int arr[100], n, key, i, found = 0;
 
-    // Input size of array
     printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    // Input array elements
     printf("Enter %d elements:\n", n);
-    for(i = 0; i < n; i++) {
+    for(i = 0; i < n; i++) 
+    {
         scanf("%d", &arr[i]);
     }
 
-    // Input the element to search
     printf("Enter element to search: ");
     scanf("%d", &key);
 
-    // Linear search
-    for(i = 0; i < n; i++) {
-        if(arr[i] == key) {
-            printf("Element %d found at position %d\n", key, i+1);
+    for(i = 0; i < n; i++) 
+    {
+        if(arr[i] == key) 
+        {
+            printf("Element %d found at position %d\n", key, i + 1);
             found = 1;
-            break;
+            break; 
         }
     }
 
-    if(!found) {
+    if(found == 0) 
+    {
         printf("Element %d not found in the array\n", key);
     }
 
