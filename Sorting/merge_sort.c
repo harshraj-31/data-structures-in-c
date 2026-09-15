688e16d443f00ec9e07e8fbd90e23ca0ecace94a
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-15 23:37:12 +0530

    Add merge sort implementation in C
    Implemented merge sort algorithm with user input for sorting an array.

--- Sorting/merge_sort.c
+++ Sorting/merge_sort.c
@@ -5,7 +5,53 @@
  */
 #include <stdio.h>
 
+void merge(int a[], int l, int m, int r) {
+    int temp[100], i = l, j = m + 1, k = 0;
+
+    // Compare and merge the two halves into temp[]
+    while (i <= m && j <= r)
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+
+    // Copy any remaining elements from the left half
+    while (i <= m) temp[k++] = a[i++];
+    
+    // Copy any remaining elements from the right half
+    while (j <= r) temp[k++] = a[j++];
+
+    // Copy the sorted elements back into the original array
+    for (i = l, k = 0; i <= r; i++, k++)
+        a[i] = temp[k];
+}
+
+void mergeSort(int a[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+        mergeSort(a, l, m);
+        mergeSort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
+
 int main() {
-    printf("Merge Sort implementation placeholder.\n");
+    int a[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    mergeSort(a, 0, n - 1);
+
+    printf("\nSorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    
+    printf("\n");
+
     return 0;
 }
