ec7914078899c47155a8dad2f0da2148028c2d1e
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-22 21:56:48 +0530

    Refactor stack operations and update result output
    Refactor push and pop functions and improve main logic.

--- Stacks/INFIX_PO.C
+++ Stacks/INFIX_PO.C
@@ -9,11 +9,15 @@ char postfix[MAX];
 int stack[MAX];
 int top = -1;
 
-void push(int x);
-int pop();
+void push(int x) {
+    stack[++top] = x;
+}
 
-int main()
-{
+int pop() {
+    return stack[top--];
+}
+
+int main() {
     int i, op1, op2, result = 0;
 
     clrscr();
@@ -21,18 +25,13 @@ int main()
     gets(postfix);
 
     for (i = 0; postfix[i] != '\0'; i++)
-    {
         if (isdigit(postfix[i]))
-        {
             push(postfix[i] - '0');
-        }
-        else
-        {
+        else {
             op2 = pop();
             op1 = pop();
 
-            switch (postfix[i])
-            {
+            switch (postfix[i]) {
                 case '+': result = op1 + op2; break;
                 case '-': result = op1 - op2; break;
                 case '*': result = op1 * op2; break;
@@ -42,19 +41,10 @@ int main()
             }
             push(result);
         }
-    }
 
-    printf("RESULT = %d", result);
+    // The final result is always the last item left on the stack
+    printf("RESULT = %d\n", stack[top]);
+    
     getch();
     return 0;
-}
-
-void push(int x)
-{
-    stack[++top] = x;
-}
-
-int pop()
-{
-    return stack[top--];
 }
