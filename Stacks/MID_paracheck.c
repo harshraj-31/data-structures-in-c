4d745e846858af924d063c1da28d1e7a03749bd6
Harshrajsinh <zalaharshrajsinh87@gmail.com>
2026-09-23 23:34:13 +0530

    Refactor stack functions and main structure

--- Stacks/MID_paracheck.c
+++ Stacks/MID_paracheck.c
@@ -6,58 +6,40 @@
 char stack[MAX];
 int top = -1;
 
-// Push function
-void push(char ch)
-{
+void push(char ch) {
     if (top == MAX - 1)
-    {
         printf("\nStack overflow");
-    }
     else
-    {
         stack[++top] = ch;
-    }
 }
 
-// Pop function
-char pop()
-{
-    if (top == -1)
-    {
+char pop() {
+    if (top == -1) {
         printf("\nStack underflow");
         return '9';
     }
-    else
-    {
-        return stack[top--];
-    }
+    return stack[top--];
 }
 
-void main()
-{
+int main() {
     char exp[MAX];
     int i, flag = 1;
     char ch;
+    
     clrscr();
 
     printf("Enter an expression: ");
     flushall();
     gets(exp);
 
-    for (i = 0; exp[i] != '\0'; i++)
-    {
+    for (i = 0; exp[i] != '\0'; i++) {
         if (exp[i] == '(' || exp[i] == '{' || exp[i] == '[')
-        {
             push(exp[i]);
-        }
-        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']')
-        {
+        else if (exp[i] == ')' || exp[i] == '}' || exp[i] == ']') {
             ch = pop();
 
-            if (ch == '9') // no opening bracket
-            {
+            if (ch == '9') 
                 flag = 0;
-            }
             else if (ch == '(' && exp[i] != ')')
                 flag = 0;
             else if (ch == '{' && exp[i] != '}')
@@ -65,15 +47,14 @@ void main()
             else if (ch == '[' && exp[i] != ']')
                 flag = 0;
         }
-
-        if (flag == 1 && top == -1)
-        {
-            printf("\n %s is valid Parentheses\n", exp);
-        }
-        else
-        {
-            printf("\n %s is not valid Parentheses\n", exp);
-        }
-
-        getch();
     }
+
+    // This validation MUST sit outside the loop!
+    if (flag == 1 && top == -1)
+        printf("\n%s is valid Parentheses\n", exp);
+    else
+        printf("\n%s is not valid Parentheses\n", exp);
+
+    getch();
+    return 0;
+}
